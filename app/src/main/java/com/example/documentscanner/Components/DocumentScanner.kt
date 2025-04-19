1432d103a038110ffaddfd6831193b7576b46545
sumit7428563756 <161425675+sumit7428563756@users.noreply.github.com>
2025-04-19 20:40:58 +0530

    first

---
+++ app/src/main/java/com/example/documentscanner/Components/DocumentScanner.kt
@@ -0,0 +1,10 @@
+package com.example.documentscanner.Components
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DocumentScanner : Application(){
+
+
+}
